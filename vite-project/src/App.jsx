61310e464f756d597ffce5552576720e6772114d
SardorDev <sardordev1@mail.ru>
2023-07-11 23:29:51 +0500

    commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [DetectedVoice, setDetectedVoice] = useState(''); // Ovoz natijasini saqlash uchun holat ustunligi
+  const [isRecognizing, setIsRecognizing] = useState(false); // Ovozni aniqlash holatini saqlash uchun holat ustunligi
+
+  const startRecognition = () => {
+    const recognition = new window.webkitSpeechRecognition() || new window.SpeechRecognition();
+    recognition.lang = 'uz-UZ'; // Ovozni aniqlash uchun tilni belgilang
+
+    setIsRecognizing(true); // Ovozni aniqlash holatini true qilish
+
+    recognition.onresult = (event) => {
+      const transcript = event.results[event.results.length - 1][0].transcript;
+      setDetectedVoice(transcript);
+      setIsRecognizing(false); // Ovozni aniqlash holatini false qilish
+    };
+
+    recognition.start();
+  };
+
+  const stopRecognition = () => {
+    setIsRecognizing(false); // Ovozni aniqlash holatini false qilish
+    setDetectedVoice(''); // Ovozni to'xtatish
+  };
+
+  const toggleRecognition = () => {
+    if (isRecognizing) {
+      stopRecognition();
+    } else {
+      startRecognition();
+    }
+  };
+
+  return (
+    <>
+      <button onClick={toggleRecognition}>
+        {isRecognizing ? 'To\'xtatish' : 'Gapiring'}
+      </button>
+      <h1>Hello World!!!</h1>
+      <h1>{DetectedVoice}</h1>
+    </>
+  );
+}
+
+export default App;
